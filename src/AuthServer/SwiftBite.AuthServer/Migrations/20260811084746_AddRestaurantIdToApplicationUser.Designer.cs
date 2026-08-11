b8231e468acf95c2f5ee36473c528aaf04f9baec
Sreedhar9652002484 <nagallisreedhar1918@gmail.com>
2026-08-11 14:20:17 +0530

    Add missing EF Core migration for ApplicationUser.RestaurantId
    RestaurantId was added to the model with no corresponding migration,
    so any query touching AspNetUsers on a freshly-migrated database
    (e.g. registration) failed with "Invalid column name 'RestaurantId'".
    Applies automatically on next deploy via the existing MigrateAsync()
    call on startup.

--- src/AuthServer/SwiftBite.AuthServer/Migrations/20260811084746_AddRestaurantIdToApplicationUser.Designer.cs
+++ src/AuthServer/SwiftBite.AuthServer/Migrations/20260811084746_AddRestaurantIdToApplicationUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SwiftBite.AuthServer.Data;
 
@@ -11,9 +12,11 @@ using SwiftBite.AuthServer.Data;
 namespace SwiftBite.AuthServer.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260811084746_AddRestaurantIdToApplicationUser")]
+    partial class AddRestaurantIdToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
